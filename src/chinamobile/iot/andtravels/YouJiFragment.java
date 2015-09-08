930d02537a84ec3972301997b635a955b5222c53
zhangphil <zhang.fei@msn.com>
2015-09-08 10:07:08 +0800

    tablelayout的布局完善

--- src/chinamobile/iot/andtravels/YouJiFragment.java
+++ src/chinamobile/iot/andtravels/YouJiFragment.java
@@ -67,67 +67,35 @@ public class YouJiFragment extends ListFragment {
 			title.setText("初游草堂，忆童年 （" + pos + "），图" + mArrayList.get(pos));
 
 			TableLayout tableLayout = (TableLayout) convertView.findViewById(R.id.tableLayout);
-			tableLayout.setStretchAllColumns(true);
+			//tableLayout.setStretchAllColumns(true);
 
 			final int WC = ViewGroup.LayoutParams.WRAP_CONTENT;
-			final int FP = ViewGroup.LayoutParams.FILL_PARENT;
+			//final int MP = ViewGroup.LayoutParams.MATCH_PARENT;
 
 			int total = mArrayList.get(pos);
-			// TableRow tableRow=new TableRow(getContext());
+		
+			int ROW=0;
+			int mod=total%3;
+			if(mod==0)
+				ROW=total/3;
+			else
+				ROW=total/3+1;
 			
 			int k = 0;
-			for (int i = 0; i < 3; i++) {
+			for (int i = 0; i < ROW; i++) {
 				TableRow tableRow = new TableRow(getContext());
+				
 				for (int j = 0; j < 3; j++) {
-					k = i * 3 + j;
 					if (k < total) {
 						View v = mLayoutInflater.inflate(R.layout.image_layout, null);
 						tableRow.addView(v);
-					}
-					else
-					{
-						tableLayout.addView(tableRow, new TableLayout.LayoutParams(FP, WC));
-						break;	
+						
+						k++;
 					}
 				}
+				
+				tableLayout.addView(tableRow, new TableLayout.LayoutParams(WC, WC));
 			}
-
-			// for(int j=0;j<3;j++){
-			//
-			// View v=mLayoutInflater.inflate(R.layout.image_layout,null);
-			// tableRow.addView(v);
-			//
-			// }
-
-			// for(int row=0;row<ROW;row++)
-			// {
-			// TableRow tableRow=new TableRow(getContext());
-			// for(int col=0;col<3;col++){
-			//
-			// View v=mLayoutInflater.inflate(R.layout.image_layout,null);
-			// tableRow.addView(v);
-			// }
-			//
-			// //新建的TableRow添加到TableLayout
-			// tableLayout.addView(tableRow, new TableLayout.LayoutParams(FP,
-			// WC));
-			// }
-
-			// gridView.setAdapter(new
-			// ImageAdapter(this.getContext(),-1,imgResId));
-
-			// RecyclerView mRecyclerView = (RecyclerView)
-			// convertView.findViewById(R.id.recyclerView);
-
-			// int spanCount = 3;
-			// RecyclerView.LayoutManager mLayoutManager = new
-			// StaggeredGridLayoutManager(spanCount,
-			// StaggeredGridLayoutManager.VERTICAL);
-			// mRecyclerView.setLayoutManager(mLayoutManager);
-
-			// RecyclerView.Adapter imageAdapter = new
-			// ImageAdapter(mArrayList.get(pos));
-			// mRecyclerView.setAdapter(imageAdapter);
 
 			return convertView;
 		}
