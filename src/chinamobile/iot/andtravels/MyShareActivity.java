83baa6d571e21f564e94e8ce8c79e495456f0849
zhangjoey1984 <zhangjoey1984@live. com>
2015-09-08 17:13:22 +0800

    在分享中添加获取服务器数据的接口

--- src/chinamobile/iot/andtravels/MyShareActivity.java
+++ src/chinamobile/iot/andtravels/MyShareActivity.java
@@ -10,6 +10,15 @@ import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.FragmentTransaction;
 import android.util.Log;
+import android.widget.Toast;
+
+import org.json.JSONObject;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
 
 public class MyShareActivity extends FragmentActivity {
 
@@ -73,6 +82,34 @@ public class MyShareActivity extends FragmentActivity {
 	@Override
 	protected void onStop() {
 		super.onStop();
+	}
+	
+	private void GetResource(){
+		
+		 String url = "http://172.16.0.11:8080/AndTravel/uservalidation/checkidentity/"; 
+		 RequestQueue mQueue = Volley.newRequestQueue(this);
+		 
+		 JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(url,null, new Response.Listener<JSONObject>() {
+		 
+		 @Override 
+		 public void onResponse(JSONObject response) {
+		    Log.d(LOG_TAG, "服务器返回客户信息是否注册"); //此处只处理景点的声音
+		    try{
+		    	if(response.getString("code").equals("1")){
+		    		//处理从服务器上获取的数据
+		    		
+		 
+		           }else{
+		        	   
+		           }
+		     }catch(Exception e){ Log.e(LOG_TAG, e.toString()); }
+			 } }, new Response.ErrorListener() {
+				 @Override 
+				 public void onErrorResponse(VolleyError error) { 
+					 Log.e(LOG_TAG,"Login Activity 没有从服务器上获取客户信息失败"); } });
+				 
+				  	 mQueue.add(jsonObjectRequest);
+		 
 	}
 
 }
