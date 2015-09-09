9a5b3b0fcdc5e3e90e7e6d0adeabe51e901eff8c
zhangphil <zhang.fei@msn.com>
2015-09-09 09:53:28 +0800

    对游记列表使用ViewHolder技术进行性能优化

--- src/chinamobile/iot/andtravels/YouJiFragment.java
+++ src/chinamobile/iot/andtravels/YouJiFragment.java
@@ -12,7 +12,6 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
-import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.TableLayout;
 import android.widget.TableRow;
@@ -20,26 +19,24 @@ import android.widget.TextView;
 
 public class YouJiFragment extends ListFragment {
 
-	private ArrayList<Integer> mArrayList;
-
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 
 		Random rand = new Random();
-		mArrayList = new ArrayList<Integer>();
+		ArrayList<Integer> mArrayList = new ArrayList<Integer>();
 		for (int i = 0; i < 50; i++) {
 			int cnt = rand.nextInt(10);
 			mArrayList.add(cnt);
 		}
-		
-		ArrayAdapter adapter = new MyArrayAdapter(getContext(), -1);
+
+		ArrayAdapter adapter = new MyArrayAdapter(getContext(), -1, mArrayList);
 		setListAdapter(adapter);
 	}
 
 	@Override
 	public void onListItemClick(ListView l, View v, int position, long id) {
-		Intent intent=new Intent(getContext(),YouJiActivity.class);
+		Intent intent = new Intent(getContext(), YouJiActivity.class);
 		startActivity(intent);
 	}
 
@@ -53,93 +50,122 @@ public class YouJiFragment extends ListFragment {
 
 	private class MyArrayAdapter extends ArrayAdapter {
 
+		private ArrayList mItems;
 		private LayoutInflater mLayoutInflater;
+		private ViewHolder holder;
 
-		public MyArrayAdapter(Context context, int resource) {
-			super(context, resource);
-			mLayoutInflater = LayoutInflater.from(context);
+		private class ViewHolder {
+			public TableLayout tableLayout;
+			public TextView title;
+			public TextView detail;
 		}
 
-		//注意！！！此处没有对性能进行优化！后期必须对此进行优化！
+		public MyArrayAdapter(Context context, int resource, ArrayList mArrayList) {
+			super(context, resource);
+			this.mLayoutInflater = LayoutInflater.from(context);
+			this.mItems = mArrayList;
+		}
+
+		// 注意！！！此处没有对性能进行优化！后期必须对此进行优化！
 		@Override
 		public View getView(int pos, View convertView, ViewGroup parent) {
-			//if(convertView==null)
+			if (convertView == null) {
 				convertView = mLayoutInflater.inflate(R.layout.youji_item, null);
-			//View view = mLayoutInflater.inflate(R.layout.youji_item, null);
 
-			TextView title = (TextView) convertView.findViewById(R.id.title);
-			title.setText("初游草堂，忆童年 （" + pos + "），图" + mArrayList.get(pos));
+				TextView title = (TextView) convertView.findViewById(R.id.title);
+				TextView detail = (TextView) convertView.findViewById(R.id.detail);
+				TableLayout tableLayout = (TableLayout) convertView.findViewById(R.id.tableLayout);
 
-			TextView detail = (TextView) convertView.findViewById(R.id.detail);
-			detail.setEllipsize(TruncateAt.END);
-			detail.setMaxLines(3);
+				holder = new ViewHolder();
 
-			TableLayout tableLayout = (TableLayout) convertView.findViewById(R.id.tableLayout);
-			//tableLayout.setStretchAllColumns(true);
+				holder.title = title;
+				holder.detail = detail;
+				holder.tableLayout = tableLayout;
 
-			final int WC = ViewGroup.LayoutParams.WRAP_CONTENT;
-			final int MP = ViewGroup.LayoutParams.MATCH_PARENT;
+				convertView.setTag(holder);
+			} else
+				holder = (ViewHolder) convertView.getTag();
 
-			int total = mArrayList.get(pos);
-		
-			int ROW=0;
-			int mod=total%3;
-			if(mod==0)
-				ROW=total/3;
-			else
-				ROW=total/3+1;
-			
-			int k = 0;
-			for (int i = 0; i < ROW; i++) {
-				TableRow tableRow = new TableRow(getContext());
-				
-				for (int j = 0; j < 3; j++) {
-					if (k < total) {
-						View v = mLayoutInflater.inflate(R.layout.image_layout, null);
-						tableRow.addView(v);
-						
-						k++;
+			holder.title.setText("初游草堂，忆童年 （" + pos + "），" + mItems.get(pos) + "图");
+
+			holder.detail.setEllipsize(TruncateAt.END);
+			holder.detail.setMaxLines(3);
+
+			int total = getItem(pos);
+
+			if (total == 0) {
+				holder.tableLayout.setVisibility(View.GONE);
+			} else {
+				holder.tableLayout.removeAllViewsInLayout();
+				holder.tableLayout.setVisibility(View.VISIBLE);
+
+				final int WC = ViewGroup.LayoutParams.WRAP_CONTENT;
+				final int MP = ViewGroup.LayoutParams.MATCH_PARENT;
+
+				int ROW = 0;
+				int mod = total % 3;
+				if (mod == 0)
+					ROW = total / 3;
+				else
+					ROW = total / 3 + 1;
+
+				int k = 0;
+				for (int i = 0; i < ROW; i++) {
+					TableRow tableRow = new TableRow(getContext());
+
+					for (int j = 0; j < 3; j++) {
+						if (k < total) {
+							View v = mLayoutInflater.inflate(R.layout.image_layout, null);
+							tableRow.addView(v);
+
+							k++;
+						}
 					}
+
+					holder.tableLayout.addView(tableRow, new TableLayout.LayoutParams(MP, WC));
 				}
-				
-				tableLayout.addView(tableRow, new TableLayout.LayoutParams(MP, WC));
 			}
 
 			return convertView;
 		}
 
 		@Override
-		public int getCount() {
-			return mArrayList.size();
-		}
-	}
-
-	public class ImageAdapter extends ArrayAdapter {
-
-		private LayoutInflater mLayoutInflater;
-		private int[] images;
-
-		public ImageAdapter(Context context, int resource, int[] images) {
-			super(context, resource);
-			mLayoutInflater = LayoutInflater.from(context);
-			this.images = images;
-		}
-
-		@Override
-		public View getView(int pos, View convertView, ViewGroup parent) {
-			if (convertView == null)
-				convertView = mLayoutInflater.inflate(R.layout.image_layout, null);
-
-			ImageView image = (ImageView) convertView.findViewById(R.id.image);
-			image.setImageResource(images[pos]);
-
-			return convertView;
+		public Integer getItem(int pos) {
+			return (Integer) mItems.get(pos);
 		}
 
 		@Override
 		public int getCount() {
-			return images.length;
+			return mItems.size();
 		}
 	}
+
+	// public class ImageAdapter extends ArrayAdapter {
+	//
+	// private LayoutInflater mLayoutInflater;
+	// private int[] images;
+	//
+	// public ImageAdapter(Context context, int resource, int[] images) {
+	// super(context, resource);
+	// mLayoutInflater = LayoutInflater.from(context);
+	// this.images = images;
+	// }
+	//
+	// @Override
+	// public View getView(int pos, View convertView, ViewGroup parent) {
+	// if (convertView == null)
+	// convertView = mLayoutInflater.inflate(R.layout.image_layout, null);
+	//
+	// ImageView image = (ImageView) convertView.findViewById(R.id.image);
+	// image.setImageResource(images[pos]);
+	//
+	// return convertView;
+	// }
+	//
+	// @Override
+	// public int getCount() {
+	// return images.length;
+	// }
+	// }
 
 }
