02dbadd24d45eaa3590285220241a19ef3a0a11a
zhangjoey1984 <zhangjoey1984@live. com>
2015-09-14 16:35:35 +0800

    提交微信功能功能

--- src/chinamobile/iot/andtravels/ShareDialog.java
+++ src/chinamobile/iot/andtravels/ShareDialog.java
@@ -2,10 +2,19 @@ package chinamobile.iot.andtravels;
 
 import java.util.HashMap;
 
+import org.json.JSONObject;
+
+import com.tencent.connect.share.QQShare;
+import com.tencent.connect.share.QzoneShare;
+import com.tencent.tauth.IUiListener;
+import com.tencent.tauth.Tencent;
+import com.tencent.tauth.UiError;
+
 import android.app.Activity;
 import android.app.Dialog;
 import android.app.ActionBar.LayoutParams;
 import android.content.Context;
+import android.content.Intent;
 import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.util.AttributeSet;
@@ -19,6 +28,7 @@ import android.widget.ImageView;
 import android.widget.PopupWindow;
 import android.widget.RadioButton;
 import android.widget.Toast;
+import chinamobile.iot.andtravels.wxapi.WXEntryActivity;
 import cn.sharesdk.framework.Platform;
 import cn.sharesdk.framework.PlatformActionListener;
 import cn.sharesdk.framework.ShareSDK;
@@ -34,51 +44,100 @@ public class ShareDialog extends Dialog {
 		super(context);
 		mContext = context;
 	}
+	private Intent intent;
+	
+	private static Tencent mTencent;
+	private String mShareQQZoneAPPId = "1104785615";
 	
 	@Override
     protected void onCreate(Bundle savedInstanceState) { 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.share_pop_window);
         
+        mTencent = Tencent.createInstance(mShareQQZoneAPPId, mContext);
+        
         shareImage1 = (RadioButton)findViewById(R.id.share1);  
 		shareImage2 = (RadioButton)findViewById(R.id.share2); 
 		shareImage3 = (RadioButton)findViewById(R.id.share3); 
 		shareImage4 = (RadioButton)findViewById(R.id.share4); 
-         
+        
+		
 		shareImage1.setOnClickListener(new View.OnClickListener() {  
             public void onClick(View v) {  
-            	showShareApp(Wechat.NAME);   
+            	//showShareApp(Wechat.NAME);  
+            	intent = new Intent(mContext,WXEntryActivity.class);
+            	intent.putExtra("shareIndex", 0);
+            	mContext.startActivity(intent);
+            	
+            	dismiss();
             }  
         }); 
 		
 		shareImage2.setOnClickListener(new View.OnClickListener() {  
             public void onClick(View v) {  
-                 
+            	intent = new Intent(mContext,WXEntryActivity.class);
+            	intent.putExtra("shareIndex", 1);
+            	mContext.startActivity(intent); 
+            	
+            	dismiss();
             }  
         }); 
 		
 		
 		shareImage3.setOnClickListener(new View.OnClickListener() {  
-            public void onClick(View v) {  
+            public void onClick(View v) { 
+            	showShareAppToQQZone();
+            	dismiss();
                  
             }  
         }); 
 		
 		shareImage4.setOnClickListener(new View.OnClickListener() {  
             public void onClick(View v) {  
+            	
+            	dismiss();
                  
             }  
         }); 
         
     }
+	/**
+	 * 分享到QQ空间的回调函数
+	 * @author JoeyZhang
+	 *
+	 */
+	private class BaseUiListener implements IUiListener {
+
+		@Override
+		public void onCancel() {
+			// TODO Auto-generated method stub
+			Log.e(LOG_TAG, "QQ空间分享时，用户选择了取消！");
+		}
+
+		@Override
+		public void onComplete(Object arg0) {
+			// TODO Auto-generated method stub
+			Log.e(LOG_TAG, "QQ空间分享成功！！！");
+		}
+
+		@Override
+		public void onError(UiError arg0) {
+			// TODO Auto-generated method stub
+			Log.e(LOG_TAG, "QQ空间分享失败，原因：" + arg0 );
+		}
+		
+	}
 	
-	private void showShareApp(String title){
+	private void showShareAppToQQZone(){
 		
-		Log.e(LOG_TAG, "点击APP分享了");
-		String APP_ID ="a55bdf374ef0";
-		ShareSDK.initSDK(mContext, APP_ID);
-		shareApp(title);
-		
+		Log.e(LOG_TAG, "点击APP分享到QQ空间");
+		//分享类型
+		final Bundle params = new Bundle();
+		params.putInt(QQShare.SHARE_TO_QQ_KEY_TYPE,QzoneShare.SHARE_TO_QZONE_TYPE_IMAGE_TEXT);
+	    params.putString(QzoneShare.SHARE_TO_QQ_TITLE, "分享APP给你的朋友吧！！！");
+	    params.putString(QzoneShare.SHARE_TO_QQ_SUMMARY, "");
+	    params.putString(QzoneShare.SHARE_TO_QQ_TARGET_URL, "http://baidu.com");
+	    mTencent.shareToQzone((Activity) mContext, params, new BaseUiListener());
 	}
 	
 	
--- src/chinamobile/iot/andtravels/wxapi/WXEntryActivity.java
+++ src/chinamobile/iot/andtravels/wxapi/WXEntryActivity.java
@@ -1,24 +1,128 @@
 package chinamobile.iot.andtravels.wxapi;
 
+import com.tencent.mm.sdk.modelbase.BaseReq;
+import com.tencent.mm.sdk.modelbase.BaseResp;
+import com.tencent.mm.sdk.modelmsg.SendMessageToWX;
+import com.tencent.mm.sdk.modelmsg.WXMediaMessage;
+import com.tencent.mm.sdk.modelmsg.WXWebpageObject;
+import com.tencent.mm.sdk.openapi.IWXAPI;
+import com.tencent.mm.sdk.openapi.IWXAPIEventHandler;
+import com.tencent.mm.sdk.openapi.WXAPIFactory;
+
 import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.RadioButton;
+import android.widget.Toast;
+import chinamobile.iot.andtravels.ContainerActivity;
 import chinamobile.iot.andtravels.R;
-import cn.sharesdk.sina.weibo.SinaWeibo;
 
-public class WXEntryActivity extends Activity {
+public class WXEntryActivity extends Activity implements IWXAPIEventHandler{
 
-	//private IWXAPI api;
+	private IWXAPI api;
 	private final String APP_ID ="wxde8390c1b8021c18";
+	private int index = 0;
 	
 	@Override
     protected void onCreate(Bundle savedInstanceState) { 
         super.onCreate(savedInstanceState);
        
-        //api = WXAPIFactory.createWXAPI(this, APP_ID, true);  
-       // api.registerApp(APP_ID); 
+        Log.e("WXEntryActivity", "进入微信分享");
+        api = WXAPIFactory.createWXAPI(this, APP_ID, true);  
+        api.registerApp(APP_ID); 
+        
+        api.handleIntent(getIntent(), (IWXAPIEventHandler) this); 
+        
+        Intent intent = getIntent();
+        Bundle bundle = intent.getExtras();
+        index = bundle.getInt("shareIndex");
         
     }
+	
+	@Override
+    protected void onStart() { 
+        super.onStart();
+       
+        Log.e("WXEntryActivity", "开始微信分享了哈");
+        
+        shareAPPToWeiXin(index);
+        
+    }
+	
+	@Override
+    protected void onResume() { 
+        super.onResume();
+        this.finish();
+    }
+	
+	
+	private void shareAPPToWeiXin(int flag) {  
+	    
+	    if (!api.isWXAppInstalled()) {  
+	        Toast.makeText(WXEntryActivity.this, "您还未安装微信客户端",  
+	                Toast.LENGTH_SHORT).show();  
+	        return;  
+	    }  
+	  
+	    WXWebpageObject webpage = new WXWebpageObject();  
+	    webpage.webpageUrl = "http://baidu.com";  
+	    WXMediaMessage msg = new WXMediaMessage(webpage);  
+	  
+	    msg.title = "分享APP";  
+	    msg.description ="分享APP给你的朋友吧！！！";  
+	    Bitmap thumb = BitmapFactory.decodeResource(getResources(),  
+	            R.drawable.a_selected);  
+	    msg.setThumbImage(thumb);  
+	    SendMessageToWX.Req req = new SendMessageToWX.Req();  
+	    req.transaction = String.valueOf(System.currentTimeMillis());  
+	    req.message = msg;  
+	    req.scene = flag;  
+	    api.sendReq(req);  
+	}  
+	
+	@Override  
+	public void onReq(BaseReq arg0) {  
+	    // TODO Auto-generated method stub 
+		this.finish();
+	}  
+	
+	@Override  
+	public void onResp(BaseResp resp) {  
+	  
+	    Log.v("WXEntryActivity", "微信分享APP结果");  
+	    String result;  
+	  
+	    
+	    switch (resp.errCode) {  
+	    case BaseResp.ErrCode.ERR_OK:  
+	        result = "分享成功";  
+	        break;  
+	    case BaseResp.ErrCode.ERR_USER_CANCEL:  
+	    	result = "";
+	        break;  
+	    case BaseResp.ErrCode.ERR_AUTH_DENIED:
+	    	result = "分享失败"; 
+	        break;  
+	    default:
+	    	result = "分享失败"; 
+	        break;  
+	    }  
+	  
+	    if(!result.isEmpty()){
+	    	Toast.makeText(this, result, Toast.LENGTH_LONG).show(); 
+	    }
+	    
+	    /*int pos = 3;
+		Intent intent = new Intent(this, ContainerActivity.class);
+		intent.putExtra("curViewPos", pos);
+		startActivity(intent);*/
+
+		this.finish();
+	  
+	}  
 
 }
